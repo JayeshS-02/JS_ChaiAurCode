7a2793157e52142bfbbd8de64de3eee3caddfd9e
JayeshS-02 <127519506+JayeshS-02@users.noreply.github.com>
2024-02-29 13:58:54 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId =144553;
+let accountEmail ='jayeshsonawane@google.com';
+var accountPassword = '123456';
+accountCity = 'Jaipur';
+
+// accountId=2525; //Not allowed
+
+accountEmail='jks@google.com';
+accountPassword='4545';
+accountCity='Pune';
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
