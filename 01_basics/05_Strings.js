5b9935f6e82ca7246bb0ac105626120f3b3b9539
JayeshS-02 <127519506+JayeshS-02@users.noreply.github.com>
2024-03-01 13:13:37 +0000

    commit

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,21 @@
+const name = "jayesh";
+const repoCount =50;
+// console.log(name+repoCount);
+
+//String interpolation
+// use ` `
+console.log(`Hello my name is ${name.toUpperCase()} and my repo count is ${repoCount}`)
+
+const gameName = new String('Jayesh Sonawane')
+console.log(gameName[0]);
+
+const str = 'Life, the universe and everything. Answer:';
+console.log(`${str} ${str.length}`);
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+
+const index = 4;
+console.log(`The character at index ${index} is ${sentence.charAt(index)}`);
+// Expected output: "The character at index 4 is q"
+
+console.log(sentence[5]);  //Returns => "u"
